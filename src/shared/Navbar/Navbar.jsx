bb27d2dc39cebaebc81037896c11a18d2fc26dc3
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-08 19:06:49 +0600

    now navbar fully responsive for all devices

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -17,45 +17,58 @@ const Navbar = () => {
     };
 
     return (
-        <div className="bg-[#faf8f5cc] relative py-4">
-            <nav className="max-w-350 mx-auto px-4 lg:px-0 flex justify-between items-center 
-                      w-full z-50">
+        <div className="bg-[#faf8f5cc] sticky py-4 ">
+            <nav className="max-w-350 mx-auto px-4 lg:px-0 flex justify-between items-center w-full z-50 ">
                 <h2 className="text-[20px] font-medium dm-serif">
                     Creative<span>w</span>ork
                 </h2>
-                {
-                    !open ?
+                {/* large devices */}
+                <ul
+                    className="hidden md:flex md:flex-row gap-4 lg:gap-6 items-center"
+                >
+                    {navLinks.map((nav) => (
+                        <li key={nav.name}>
+                            <NavLink
+                                to={nav.path}
+                                className="dm-sans text-sm text-[#91877d] hover:text-gray-700 transition-all font-medium"
+                            >
+                                {nav.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
 
-                        <ul className="absolute md:relative w-full md:w-auto 
-               flex flex-col md:flex-row 
-               bg-[#faf8f5cc] md:bg-transparent 
-               gap-6 md:gap-4 lg:gap-6 
-               items-center 
-               top-16 md:top-0 left-0 py-6 md:py-0">
-                            {navLinks.map((nav) => (
-                                <li key={nav.name}>
-                                    <NavLink
-                                        to={nav.path}
-                                        className="dm-sans text-sm text-[#91877d] hover:text-gray-700 transition-all font-medium"
-                                    >
-                                        {nav.name}
-                                    </NavLink>
-                                </li>
-                            ))}
-                        </ul>
-                        :
-                        ""
-                }
+                {/* small devices */}
+                <ul
+                    className={`absolute md:hidden w-full flex flex-col bg-[#faf8f5cc] gap-6 items-center left-0 py-6 transform transition-all duration-300 ease-in-out
+
+                    ${open
+                            ? "top-16 opacity-100 -z-40"
+                            : "-top-60 opacity-0 -z-40"
+                    }`}
+                >
+                    {navLinks.map((nav) => (
+                        <li key={nav.name}>
+                            <NavLink
+                                to={nav.path}
+                                className="dm-sans text-sm text-[#91877d] hover:text-gray-700 transition-all font-medium"
+                            >
+                                {nav.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+                
                 <div className="flex items-center gap-4">
                     <div className="rounded-full hover:bg-white text-[#91877d] hover:text-gray-700 transition-all p-2">
                         <GrSearch className="dm-sans text-lg font-medium" />
                     </div>
-                    <div onClick={() => {handleMenu()}} className="block md:hidden rounded-full hover:bg-white text-[#91877d] hover:text-gray-700 transition-all p-2">
+                    <div onClick={() => { handleMenu() }} className="block md:hidden rounded-full hover:bg-white text-[#91877d] hover:text-gray-700 transition-all p-2">
                         {
                             open ?
-                                <AiOutlineMenu className="dm-sans text-xl font-medium" />
-                                :
                                 <IoClose className="dm-sans text-xl font-medium" />
+                                :
+                                <AiOutlineMenu className="dm-sans text-xl font-medium" />
                         }
                     </div>
                 </div>
