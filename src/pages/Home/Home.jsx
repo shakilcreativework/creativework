28015ba0d591a78771c041db6db14dc595257b1f
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-18 19:50:45 +0600

    now, try add text show perfectly for banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,10 +43,10 @@ const Home = () => {
         <div>
             <div className="bg-[linear-gradient(to_right,rgba(255,255,255,0.1),rgba(224,122,95,0.35),rgba(255,255,255,0.05)),url('https://i.ibb.co.com/7dZ65ZpV/arts-6.jpg')] bg-cover bg-center h-100 md:h-108.75 lg:h-134 xl:h-140 w-full">
                 <Container className={"h-full"}>
-                    <div className="flex flex-col gap-4 justify-center items-start h-full">
-                        <h1 className=" text-3xl md:text-5xl lg:text-6xl text-black/80 dm-serif text-shadow-lg">Curated Design Resources</h1>
+                    <div className="flex flex-col gap-2 md:gap-4 justify-center items-start h-full">
+                        <h1 className=" text-[28px] md:text-5xl lg:text-6xl text-black/80 dm-serif text-shadow-lg">Curated Design Resources</h1>
                         <p className=" text-sm md:text-base lg:text-lg text-black/80 text-shadow-lg">Discover premium backgrounds, mockups, and design assets for your creative projects.</p>
-                        <div className="flex flex-col md:flex-row gap-3">
+                        <div className="flex flex-col md:flex-row gap-3 mt-2 md:mt-0">
                             <MagneticWrapper text={"Explore Designs"} className={'bg-black text-white'} rightIcon={<FaArrowRightLong />} to={'/branding'} as="link" />
                             <MagneticWrapper text={"Browse Backgrounds"} className={'text-black'} to={'/backgrounds'} as="link" />
                         </div>
