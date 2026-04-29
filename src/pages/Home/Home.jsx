1192fa67f14e926b0a65fe1ec3fff8ec06f706ee
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-29 14:13:27 +0600

    now clean unnecesary components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,6 @@ import DesignersChoose from "../../components/DesignersChoose/DesignersChoose";
 import { BsFire } from "react-icons/bs";
 import TrendingPost from "../../components/TrendingPost/TrendingPost";
 import FreeBundle from "../../components/FreeBundle/FreeBundle";
-// import Projects from "../../components/Projects/Projects";
-// import GithubStats from "../../components/GithubStats/GithubStats";
 
 
 
@@ -52,11 +50,6 @@ const Home = () => {
             <div>
                 <DontLeave />
             </div>
-
-            {/* testing portfolio project card live preview */}
-            {/* <Projects />
-
-            <GithubStats /> */}
         </div>
     );
 };
